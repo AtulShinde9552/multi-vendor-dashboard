e4781842e39d86d6d8a99611f86f25f41cd6dd91
AtulShinde9552 <113991404+AtulShinde9552@users.noreply.github.com>
2024-06-12 01:50:42 +0530

    NA

---
+++ src/api/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const local = 'http://localhost:5000'
+const production = 'https://multi-vendor-backend-lywh.onrender.com'
+
+let api_url = ''
+let node = 'pro'
+
+if (node === 'pro') {
+    api_url = production
+
+}else{
+    api_url = local
+}
+
+
+const api = axios.create({
+   baseURL: `${api_url}/api`,
+   withCredentials: true
+})
+export default api
